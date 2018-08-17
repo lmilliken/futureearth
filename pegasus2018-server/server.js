6b23deb73a610f34c36119f0ada499c0fc0d0f9e
lmilliken <millifly@gmail.com>
2018-08-17 01:22:06 -0600

    making progress on file uploads

--- pegasus2018-server/server.js
+++ pegasus2018-server/server.js
@@ -2,18 +2,50 @@ const express = require('express')
 const fs = require('fs')
 var cors = require('cors');
 var bodyParser  = require('body-parser');
+const multer = require('multer');
+const path = require('path')
+//Set Storage Enginee
+const storage = multer.diskStorage({
+    destination: './public/uploads',
+    filename: function(req, file, cb){
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+
+//Init Upload
+const upload = multer({
+    storage: storage
+})
 
 const port = process.env.PORT || 8081; 
 
 var app = express();
+//Public Folder
+app.use(express.static('./public'))
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/submit', (req, res)=>{
-    console.log("salsa is here: " , req.body)
-    console.log("type of file: ", typeof req.body.uploadProposal)
-    res.send('OK')
+
+app.post('/submit', upload.any(), (req, res)=>{
+    console.log(req)
+    // uploadProposal(req,res, (err)=>{
+    //     if(err){
+    //         console.log('error you fool! ', err)
+    //     } else {
+    //         console.log(req)
+    //         res.send('test')
+    //     }
+    // })
+
+    // uploadBudget(req,res, (err)=>{
+    //     if(err){
+    //         console.log('error you fool! ', err)
+    //     } else {
+    //         console.log(req)
+    //         res.send('test')
+    //     }
+    // })
 })
 
 app.listen(port, ()=>{
