676a55cab175faf803b69a49e73d7f6f7611ae64
lmilliken <millifly@gmail.com>
2018-08-17 01:52:57 -0600

    files can be pulled up in heroku app!

--- pegasus2-form-react/src/App.js
+++ pegasus2-form-react/src/App.js
@@ -122,9 +122,11 @@ class App extends Component {
     return (
       <div className="App container">
         <head>
-        {/* <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"/> */}
+        {/* <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"/>  http://localhost:8081/submit*/}
         </head>
-      <form enctype="multipart/form-data" action="http://localhost:8081/submit" method="POST">
+
+        
+      <form enctype="multipart/form-data" action="https://rocky-harbor-45956.herokuapp.com/submit" method="POST">
          <div class="container form-row">
                     <div class="form-group col-md-12">        
                         <label for="proposalTitle">Full Title of Proposal:</label>
