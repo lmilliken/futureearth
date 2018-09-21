822fcf4747d793688bd9ab3b622fc681ae14b165
lmilliken <millifly@gmail.com>
2018-09-20 20:04:21 -0600

    feable attempt to restore things

---
+++ pegasus2018-server/utils/multer-client.js
@@ -0,0 +1,50 @@
+const multer = require("multer");
+const path = require("path");
+
+var uploadLocal = (req, res) => {
+  console.log("body", req.body);
+  return new Promise((resolve, reject) => {
+    //Set Storage Enginee
+    const storage = multer.diskStorage({
+      destination: "./public/uploads",
+      filename: function(req, file, cb) {
+        //figure out if there's more than one investigator, if there is take the 1st of the array as the last name
+        let lastName = "";
+        req.body.lastName.isArray
+          ? (lastName = req.body.lastName[0])
+          : (lastName = req.body.lastName);
+
+        cb(
+          null,
+          lastName +
+            "_" +
+            file.fieldname.substring(6) +
+            "-" +
+            Date.now() +
+            path.extname(file.originalname)
+        );
+      }
+    });
+    //Init Upload
+    const upload = multer({
+      storage: storage
+    }).fields([{ name: "uploadProposal" }, { name: "uploadBudget" }]);
+
+    upload(req, res, err => {
+      if (err) {
+        reject("Multer error uploading to local storage: ", err);
+      } else {
+        console.log("files in Multer", req.files);
+        if (
+          req.files.uploadProposal[0].mimetype != "application/pdf" ||
+          req.files.uploadBudget[0].mimetype != "application/pdf"
+        ) {
+          resolve("Invalid File");
+        }
+        resolve();
+      }
+    });
+  });
+};
+
+module.exports = { uploadLocal };
