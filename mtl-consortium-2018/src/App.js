be3d5a6f49739a330a5155da3e7261177e80c230
lmilliken <millifly@gmail.com>
2018-10-03 13:59:38 -0600

    added react-select

--- mtl-consortium-2018/src/App.js
+++ mtl-consortium-2018/src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
-import Card from "./components/Card";
 import Select from "react-select";
+import Card from "./components/Card";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { institutions: [1, 2, 3] };
-    // this.handleAdd = this.handleAdd.bind(this);
+    this.state = {
+      searchResults: [],
+      keywords: [],
+      selectedTheme: [],
+      selectedLead: []
+    };
+    this.handleThemes = this.handleThemes.bind(this);
+    this.handleLeads = this.handleLeads.bind(this);
+    this.search = this.search.bind(this);
   }
+
+  search() {
+    console.log("search clicked");
+    console.log(this.state.selectedTheme), console.log(this.state.selectedLead);
+  }
+
+  handleLeads = selectedLead => {
+    this.setState({ selectedLead });
+    console.log(`Option selected:`, selectedLead);
+  };
+
+  handleThemes = selectedTheme => {
+    this.setState({ selectedTheme });
+    console.log(`Option selected:`, selectedTheme);
+  };
 
   async componentWillMount() {
     // const data = await this.getInstitutions();
@@ -30,6 +51,7 @@ class App extends Component {
     //   return <Card {...member} key={member._id} />;
     // });
 
+    console.log("state", this.state);
     const themeOptions = [
       { value: "K_CP", label: "Consumption & Production" },
       { value: "K_DECARB", label: "Decarbonisation" },
@@ -42,6 +64,40 @@ class App extends Component {
       { value: "K_URBAN", label: "Urban" }
     ];
 
+    const leadOptions = [
+      {
+        value: "École de technologie supérieure",
+        label: "École de technologie supérieure"
+      },
+      {
+        value: "École polytechnique de Montréal",
+        label: "École polytechnique de Montréal"
+      },
+      { value: "HEC Montréal", label: "HEC Montréal" },
+      { value: "Université Laval", label: "Université Laval" },
+      { value: "Université de Montréal", label: "Université de Montréal" },
+      { value: "Université de Sherbrooke", label: "Université de Sherbrooke" },
+      { value: "Université du Québec", label: "Université du Québec" },
+      {
+        value: "Université du Québec à Chicoutimi",
+        label: "Université du Québec à Chicoutimi"
+      },
+      {
+        value: "Université du Québec à Montréal",
+        label: "Université du Québec à Montréal"
+      },
+      {
+        value: "Université du Québec en Abitibi-Témiscamingue",
+        label: "Université du Québec en Abitibi-Témiscamingue"
+      },
+      { value: "McGill Universit", label: "McGill University" },
+      { value: "Concordia University", label: "Concordia University" },
+      { value: "INRS", label: "INRS" },
+      {
+        value: "Université du Québec à Rimouski",
+        label: "Université du Québec à Rimouski"
+      }
+    ];
     // console.log(Object.keys(themes));
     // Object.keys(themes).map(function(key, index) {
     //   console.log(key + ": " + themes[key]);
@@ -51,19 +107,18 @@ class App extends Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
+          <div className="col-md-3">
             <input
               type="text"
-              class="form-control"
-              placeholder="We want a search box for keywords, right?"
+              className="form-control"
+              placeholder="Keywords"
               aria-describedby="basic-addon2"
             />
-            <button className="btn">Clear Filters</button>
           </div>
           <div className="col-md-4">
             <Select
               // value={selectedOption}
-              // onChange={this.handleChange}
+              onChange={this.handleThemes}
               isMulti={true}
               options={themeOptions}
               placeholder="Themes"
@@ -71,13 +126,25 @@ class App extends Component {
           </div>
 
           <div className="col-md-4">
-            <div>Lead Institutions</div>
+            <Select
+              // value={selectedOption}
+              onChange={this.handleLeads}
+              isMulti={true}
+              options={leadOptions}
+              placeholder="Lead Institutions"
+            />
+          </div>
+          <div className="col-md-1">
+            <button className="btn btn-primary" disabled onClick={this.search}>
+              Search
+            </button>
           </div>
         </div>
 
         <div className="gridContainer" style={constainerStyle}>
           {/* {cards} */}
         </div>
+        <div>Search for institutions.</div>
       </div>
     );
   }
